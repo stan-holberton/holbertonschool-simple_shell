d9bebb6d414c9f1a62c04768f220150093498b57
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-01-08 21:15:05 +0100

    Removal of the $ symbol in non-interactive mode

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "main.h"
 
 #define PROMPT "$ " /* Définition de l'invite de commande */
+
 /**
  * main - Fonction principale d'un interpréteur de commandes simple.
  *
@@ -12,39 +13,38 @@
  */
 int main(void)
 {
-	char *userInput = NULL; /* Chaîne pour stocker l'entrée utilisateur */
-	size_t inputBufferSize = 0; /* Taille du tampon pour getline */
-	ssize_t bytesRead; /* Nombre de caractères lus par getline */
-	char **commandArguments; /* Tableau pour stocker les arguments */
-	size_t argIndex; /* Index pour parcourir et libérer les arguments */
-		while (1) /* Boucle infinie pour traiter les commandes en continu */
+	char *userInput = NULL;
+	size_t inputBufferSize = 0;
+	ssize_t bytesRead;
+	char **commandArguments;
+	size_t argIndex;
+	int interactive;
+
+	interactive = isatty(STDIN_FILENO);
+
+	while (1)
 	{
-		/* Affichage de l'invite de commande */
-		write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-		/* Lecture de l'entrée utilisateur */
+		if (interactive)
+			write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+
 		bytesRead = getline(&userInput, &inputBufferSize, stdin);
-		if (bytesRead == -1) /* Vérification de fin de fichier ou d'erreur */
+		if (bytesRead == -1)
 		{
-			free(userInput); /* Libération de la mémoire de l'entrée utilisateur */
-			write(STDOUT_FILENO, "\n", 1); /* Ajout d'une nouvelle ligne */
+			free(userInput);
+			if (interactive)
+				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-		/* Suppression du caractère de nouvelle ligne à la fin de l'entrée */
 		userInput[strcspn(userInput, "\n")] = '\0';
-		/* Si l'entrée est vide, on retourne au début de la boucle */
 		if (strlen(userInput) == 0)
 			continue;
-		/* Découpe l'entrée en arguments de commande */
 		commandArguments = split_line(userInput);
-		if (!commandArguments) /* Vérification de l'échec de la découpe */
+		if (!commandArguments)
 			continue;
-		/* Exécute la commande spécifiée par l'utilisateur */
 		execute(commandArguments);
-		/* Libération de la mémoire des arguments */
 		for (argIndex = 0; commandArguments[argIndex] != NULL; argIndex++)
-			free(commandArguments[argIndex]); /* Libère chaque argument */
-		free(commandArguments); /* Libère le tableau des arguments */
+			free(commandArguments[argIndex]);
+		free(commandArguments);
 	}
-	return (0); /* Indique que le programme s'est terminé avec succès */
+	return (0);
 }
-
